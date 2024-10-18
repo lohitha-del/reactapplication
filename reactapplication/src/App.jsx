5a580f64df4e2390d703c9adb04c8ed305699b68
lohitha-del <22R91A1284@tkrec.ac.in>
2024-10-18 14:42:03 +0530

    first commit

---
+++ reactapplication/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (  
+    <>
+    <h1>FAMILY RESTAURANT</h1>
+    
+    <ul>
+        <li>BUNNY</li>
+        <li>9876543210</li>
+        <li>bunny@gmail.com</li>
+        <li>lbnagar,hyderabad,telangana</li>
+        <li>pincode:500097</li>
+    </ul>
+    
+
+    </>
+  )
+}
+
+export default App
